f057bcccc5068b22c3d36f1d0a5e93b9c95a7abc
عبدالرحمن علي خالد عباس <92335191+AbdoAli2002a@users.noreply.github.com>
2026-05-02 23:12:08 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react"; // تم تغييرها من swc للنسخة القياسية
+import react from  "@vitejs/plugin-react": "^5.0.4", // تم تغييرها من swc للنسخة القياسية
 import path from "path";
-
+   
 export default defineConfig({
   plugins: [react()],
   resolve: {
